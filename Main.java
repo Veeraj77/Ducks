174b14c4ea9e145cb8a0f2b8ab6c6b28cd372b99
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-06 23:56:18 +0530

    creating Mallardduck object and Declaring an Instance

--- Main.java
+++ Main.java
@@ -3,3 +3,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        MallardDuck md= new MallardDuck();
+        System.out.println("----------");
+        md.display();
+        md.performQuack();
+        md.performSwim();
+        md.performFly();
+        System.out.println("----------");
+
+
