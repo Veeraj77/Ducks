0d1bef4cbc3ee758cd1f037152b60211a8beb8b5
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-07 00:28:56 +0530

    creating setQuackBehaviour

--- Duck.java
+++ Duck.java
@@ -2,3 +2,7 @@ abstract public class Duck {
     QuackBehaviour quackBehaviour;
     SwimBehaviour swimBehaviour;
     FlyingBehaviour flyingBehaviour;
+    public void setQuackBehaviour(QuackBehaviour quackBehaviour) {
+        this.quackBehaviour = quackBehaviour;
+    }
+
